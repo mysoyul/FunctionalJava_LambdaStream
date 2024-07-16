86e8e9955464c5b93d7fed9e277033c6f3d61d4b
mysoyul <vega2k_@naver.com>
2024-07-16 16:31:11 +0900

    ParallelStreams update

--- src/main/java/lambdasinaction/_02stream/parallel/ParallelStreams.java
+++ src/main/java/lambdasinaction/_02stream/parallel/ParallelStreams.java
@@ -3,7 +3,7 @@ package lambdasinaction._02stream.parallel;
 import java.util.stream.*;
 
 public class ParallelStreams {
-
+    //기존 For Loop
     public static long iterativeSum(long n) {
         long result = 0;
         for (long i = 0; i <= n; i++) {
@@ -11,29 +11,45 @@ public class ParallelStreams {
         }
         return result;
     }
-
+    //순차스트림 - 크기가 고정 되지 않고, Unboxing 비용이 발생하는 스트림
     public static long sequentialSum(long n) {
-        return Stream.iterate(1L, i -> i + 1).limit(n).reduce(Long::sum).get();
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 
+    //병렬스트림 - 크기가 고정 되지 않고, Unboxing 비용이 발생하는 스트림
     public static long parallelSum(long n) {
-        return Stream.iterate(1L, i -> i + 1).limit(n).parallel().reduce(Long::sum).get();
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 
+    //순차스트림 - 크기가 고정된 LongStream
     public static long rangedSum(long n) {
-        return LongStream.rangeClosed(1, n).reduce(Long::sum).getAsLong();
+        return LongStream.rangeClosed(1, n)
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 
+    //병렬스트림 - 크기가 고정된 LongStream
     public static long parallelRangedSum(long n) {
-        return LongStream.rangeClosed(1, n).parallel().reduce(Long::sum).getAsLong();
+        return LongStream.rangeClosed(1, n)
+                .parallel()
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 
+    //순차스트림 - Accumulator 사용
     public static long sideEffectSum(long n) {
         Accumulator accumulator = new Accumulator();
         LongStream.rangeClosed(1, n).forEach(accumulator::add);
         return accumulator.total;
     }
-
+    //병렬스트림 - Accumulator 사용, 동시성 문제 발생함
     public static long sideEffectParallelSum(long n) {
         Accumulator accumulator = new Accumulator();
         LongStream.rangeClosed(1, n).parallel().forEach(accumulator::add);
