cd27fe52ad27a535904a0338b1c5c5df9a071804
mysoyul <vega2k_@naver.com>
2024-07-16 11:13:01 +0900

    MobileTesterWithOptional update

--- src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
+++ src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
@@ -17,6 +17,14 @@ public class MobileTesterWithOptional {
 		Mobile mobile2 = new Mobile(2015001, "Apple", "iPhone 6s", Optional.empty());		
 		int width2 = mService.getMobileScreenWidth(Optional.of(mobile2));
 		System.out.println("Apple iPhone 16s Screen Width = " + width2);
+
+		//orElseGet(Supplier)
+		Optional<DisplayFeatures> displayFeaturesOptional = mobile2.getDisplayFeatures();
+		DisplayFeatures displayFeatures = displayFeaturesOptional
+				.orElseGet(() -> new DisplayFeatures("0.0", Optional.empty()));
+		System.out.println(displayFeatures.getSize());
+
+		//orElseThrow(Supplier X) X extends Throwable
 		
 	}
 
